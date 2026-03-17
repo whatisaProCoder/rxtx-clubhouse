ea21f1f88805a716cb3f909de75a1bff7308d5ec
whatisaProCoder <debnathpritam2005@gmail.com>
2026-03-17 07:33:01 +0000

    working on signup and auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
+const passport = require("passport");
 const db = require("../db/queries");
 
-const { body, validateResult, matchedData } = require("express-validator");
+const { body, validationResult, matchedData } = require("express-validator");
 
 const cannotBeEmpty = "cannot be empty";
 const maxChars = (maxLength) => `must be within ${maxLength} characters`;
@@ -36,22 +37,57 @@ const validateUserSignUp = [
     .withMessage("Passwords don't match!")
 ]
 
-const validateUserLogIn = [
-  body("email")
-    .trim()
-    .notEmpty().withMessage("E-Mail " + cannotBeEmpty)
-    .isEmail().withMessage("E-Mail is not valid")
-    .isLength({ max: 255 }).withMessage("E-mail " + maxChars(255)),
-  body("password")
-    .trim()
-    .notEmpty().withMessage("Password field " + cannotBeEmpty)
-    .isLength({ max: 255 }).withMessage("Password " + maxChars(255)),
-]
-
 exports.signUpPageGet = (req, res) => {
   res.render("sign-up");
 }
 
+exports.signUpPagePost = [
+  validateUserSignUp,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render(
+        "sign-up",
+        {
+          errors: errors.array(),
+          oldData: req.body
+        }
+      )
+    }
+
+    const { firstName, lastName, email, password } = matchedData(req);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      await db.addUser({ firstName, lastName, email, hashedPassword });
+    } catch (error) {
+      next(error);
+    }
+
+    res.redirect("/log-in");
+  }
+]
+
 exports.loginPageGet = (req, res) => {
   res.render("log-in");
 }
+
+exports.loginPagePost = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+
+    if (!user) {
+      return res.render("log-in", {
+        errors: [{ msg: info.message }],
+        data: { email: req.body.email }
+      });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect("/");
+    });
+  })(req, res, next);
+};
--- db/queries.js
+++ db/queries.js
@@ -9,3 +9,10 @@ exports.getUserUsingID = async (id) => {
   const { rows } = await pool.query("SELEECT * FROM users WHERE id = $1", [id]);
   return rows[0];
 }
+
+exports.addUser = async ({ firstName, lastName, email, hashedPassword }) => {
+  await pool.query(
+    "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
+    [firstName, lastName, email, hashedPassword]
+  );
+}
